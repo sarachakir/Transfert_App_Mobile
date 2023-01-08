98585ca357c3c56e294a8b59be9b1a289aa47462
Soukiana <Mobile-Prj>
2023-01-08 09:52:54 +0100

    login some modifications

--- app/src/main/java/com/example/ensa_transfert/MainActivity.java
+++ app/src/main/java/com/example/ensa_transfert/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getAccessToken();
+                //getAccessToken();
                 //??????
                 openHomeActivity();
             }
